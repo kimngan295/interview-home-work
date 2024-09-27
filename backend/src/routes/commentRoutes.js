bc3c4fd322535b14ad69fe7e72cd4a5efba30719
kimngan295 <kimngan040201@gmail.com>
2024-09-27 19:09:47 +0700

    fix refresh token and add route for feature comment, post

--- backend/src/routes/commentRoutes.js
+++ backend/src/routes/commentRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import authenticate from '../middleware/authenticateToken.js';
-import { createNewComment, updateComment } from '../controllers/commentController.js';
+import { createNewComment, deleteComment, updateComment } from '../controllers/commentController.js';
 
 const router = express.Router();
 
 router.post('/:postID/comment', authenticate, createNewComment);
-router.post('/update-comment/:commentID', authenticate, updateComment)
+router.post('/update-comment/:commentID', authenticate, updateComment);
+router.post('/delete-comment/:commentID', authenticate, deleteComment)
 
 export default router;
--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { createPost, getAllPosts, searchPostByTitle, updatePost } from '../controllers/postController.js';
+import { createPost, deletePost, getAllPosts, getPostDetails, searchPostByTitle, updatePost } from '../controllers/postController.js';
 import authenticate from '../middleware/authenticateToken.js';
 
 const router = express.Router();
 
 router.get('/', getAllPosts);
 router.post('/new-post', authenticate, createPost);
-router.post('/update-post/:postID',authenticate, updatePost);
-router.post('/search', searchPostByTitle)
+router.post('/update-post/:postID', authenticate, updatePost);
+router.post('/search', searchPostByTitle);
+router.post('/:postID', authenticate, getPostDetails);
+router.post('delete-post/:postID', authenticate, deletePost);
 
 export default router;
